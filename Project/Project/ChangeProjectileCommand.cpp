d4058e6086654495b4dc0fd8e58e22aa8c71ed87
Mabaria <magnomrikard@gmail.com>
2017-10-15 16:40:54 +0200

    Added tap to change projectile command

--- Project/Project/ChangeProjectileCommand.cpp
+++ Project/Project/ChangeProjectileCommand.cpp
@@ -10,6 +10,7 @@ ChangeProjectileCommand::ChangeProjectileCommand(Tank* tank1, Tank* tank2)
 
 void ChangeProjectileCommand::execute(bool & tapKey)
 {
+	tapKey = true;
 	this->tank1->changeProjectile();
 	this->tank2->changeProjectile();
 }
